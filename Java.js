1eda60772fb8cffde18d7f1969c971844ce5109a
John Franzese <johnfranzese21@gmail.com>
2024-02-22 13:51:27 -0600

    HTML

---
+++ Java.js
@@ -0,0 +1,31 @@
+//Exercise 1
+const firstName = "Derrick";
+let lastName = "Rose";
+let age;
+
+//Exercise 2
+// let firstName = "Derrick";
+// let lastName = "Rose";
+age = 35;
+
+//Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+//JavaScript Exercise is in HTML Exercise
